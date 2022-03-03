6140c4d32852db04a155dbc6e2777179f1b39e05
Jokyeon <jhonathan.fs@hotmail.com>
2022-03-03 15:40:42 -0300

    Correção de bug

--- condicional/Atividade2.java
+++ condicional/Atividade2.java
@@ -16,34 +16,40 @@ public class Atividade2 {
 			System.out.print("Insira o terceiro número: ");
 				C = leia.nextInt();
 				
-				if (A >= B && A >= C) {
-					if (B >= C) 	{
-						System.out.println("A Ordem é " + C +
-								" - "+ B +" - " + A + ".");
-					} else {
-						System.out.println("A Ordem é " + B +
-								" - " + C + " - " + A + ".");
+				
+				if (A != B && A != C && B != C ) {
+					if (A >= B && A >= C) {
+						if (B >= C) 	{
+							System.out.println("A Ordem é " + C +
+									" - "+ B +" - " + A + ".");
+						} else {
+							System.out.println("A Ordem é " + B +
+									" - " + C + " - " + A + ".");
+						}
+					}	
+					
+					if (B >= A && B >= C) {
+						if (A >= C) {
+							System.out.println("A Ordem é " + C +
+									" - " + A + " - " + B + ".");
+						} else {
+							System.out.println("A Ordem é " + A +
+									" - " + C + " - " + B + ".");
+						}
 					}
-				}	
 			
-				if (B >= A && B >= C) {
-					if (A >= C) {
-						System.out.println("A Ordem é " + C +
-								" - " + A + " - " + B + ".");
-					} else {
-						System.out.println("A Ordem é " + A +
-								" - " + C + " - " + B + ".");
+					if (C >= B && C >= A) {
+						if (A >= B) {
+							System.out.println("A Ordem é " + B +
+									" - " + A + " - " + C +".");
+						} else {
+							System.out.println("A Ordem é " + A +
+									" - " + B + " - " + C + ".");
+						}
 					}
-				}
-			
-				if (C >= B && C >= A) {
-					if (A >= B) {
-						System.out.println("A Ordem é " + B +
-								" - " + A + " - " + C +".");
-					} else {
-						System.out.println("A Ordem é " + A +
-								" - " + B + " - " + C + ".");
-					}
+				} else {
+					System.out.println("A Ordem é " + A +
+									" - " + B + " - " + C + ".");
 				}
 	}
 }
